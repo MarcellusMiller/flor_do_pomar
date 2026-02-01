bf1315695f45bc96147d9cb35f460f8a2c761719
Marcellus <Marcelomiller557@gmail.com>
2026-02-01 16:04:37 -0300

    adicionada lib de email e um teste basico de envio

--- app/src/router.ts
+++ app/src/router.ts
@@ -4,6 +4,7 @@ import uploadMiddleware from "./middleware/uploadMiddleware.js";
 import adminController from "./controller/admin/adminController.js";
 import { adminAuth } from "./middleware/adminMiddleware.js";
 import adminLoginController from "./controller/admin/adminLoginController.js";
+import sendTestEmailController from "./controller/email/sendTestEmailController.js";
 const router = Router();
 
 // rotas da aplicação
@@ -16,7 +17,8 @@ router.get("/", (req, res) => {
 // rotas da api
 router.post("/message",
     uploadMiddleware.array("image", 5),
-    messageController.createMessage
+    messageController.createMessage,
+    sendTestEmailController.send
 );
 
 router.get("/admin/messages", 
@@ -41,6 +43,10 @@ router.delete("/admin/delete/messages/:id",
 
 router.post("/admin/login",
     adminLoginController.login
+)
+
+router.post("/email/sendTest",
+    sendTestEmailController.send
 )
 
 export default router;
