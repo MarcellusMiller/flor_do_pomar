6220f3cf090a838f82ced9fc7fb4bc80b282450c
Marcellus <Marcelomiller557@gmail.com>
2026-02-12 13:12:49 -0300

    ajuste na entrega estatica das imagens

--- app/src/app.ts
+++ app/src/app.ts
@@ -15,12 +15,12 @@ const app = express();
 
 // configurando o app para receber requisições json
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(coockieParser());
 
 // Arquivos estáticos primeiro
-app.use("/images", express.static(path.join(__dirname, "..", "storage", "images")));
-app.use("/gallery", express.static(path.join(__dirname, "..", "storage", "gallery")));
+app.use("/images", express.static(path.join(process.cwd(), "storage", "images")));
+app.use("/gallery", express.static(path.join(process.cwd(), "storage", "gallery")));
 
 // Depois as rotas
 app.use(router);
